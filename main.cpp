d9f2a759835a2e03fd73f63515dde83bd51f9c22
Malcottan <maxtryasiy@gmail.com>
2023-03-08 11:20:21 +0200

    #1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float x;
+    float y;
+    cout << "x" << endl;
+    cout << "Enter the number(x): ";
+    cin >> x;
+    cout << "y" << endl;
+    cout << "Enter the number(y): ";
+    cin >> y;
+    cout << "Output number: " << (x+y)/2 << endl;
+    return 0;
+}
